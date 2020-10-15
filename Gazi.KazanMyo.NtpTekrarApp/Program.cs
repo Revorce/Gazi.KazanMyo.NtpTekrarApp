09ddf70b554424d594bb7cb9a6bd57bcd00b27f3
Berkay Sezgin <berkay.sez06@hotmail.com>
2020-10-15 19:43:27 +0300

    Proje dosyası ekle.

---
+++ Gazi.KazanMyo.NtpTekrarApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using OgrenciLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazi.KazanMyo.NtpTekrarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci();
+            ogrenci.ad = "Berkay";
+            ogrenci.soyad = "Sezgin";
+            ogrenci.telno = "05457345711";
+            ogrenci.yas = 20;
+            ogrenci.Ortalama = 3;
+            //Ortalama 2'den büyükse, basşarılı değilse başarısız yazsın
+
+            // ogrenci.Basaridurum = "basarili";//set
+            Console.WriteLine(ogrenci.Basaridurum);//get
+            Console.ReadKey();
+        }
+    }
+    
+   
+}
---
+++ OgrenciLib/Ogrenci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OgrenciLib
+{
+    public class Ogrenci
+    {
+        public string ad;
+        public string soyad;
+        public string telno;
+        public byte yas;
+        string basaridurum;//Field
+        double ortalama;//Field
+
+        public string Basaridurum { get => basaridurum; }//(Readonly) Property
+        public double Ortalama//Property
+        {
+            get
+            {
+                return Ortalama;
+            }
+            set
+            {
+                ortalama = value;
+                if (value > 2)
+                {
+                    basaridurum = "Başarılı";
+                }
+                else
+                {
+                    basaridurum = "Başarısız";
+                }
+            }
+        }
+    }
+}
